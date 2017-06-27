a56a0d26d1cf2abdf96337a16c441b16cfd51208
Simon SICARD <ssicard@e2r7p1.42.fr>
2017-06-27 11:29:52 +0200

    done

--- computor.py
+++ computor.py
@@ -6,7 +6,7 @@ def sqrt(x):
     last_guess= x/2.0
     while True:
         guess= (last_guess + x/last_guess)/2
-        if abs(guess - last_guess) < .000001: # example threshold
+        if abs(guess - last_guess) < .000001: #threshold
             return guess
         last_guess= guess
 
@@ -18,7 +18,7 @@ elif sys.argv[1] == '':
     sys.exit()
 else:
     eq = str(sys.argv[1])
-    print '\nthe equation is the following: ' + eq
+    #print '\nthe equation is the following: ' + eq
 
 c0 = 0;
 c1 = 0;
@@ -26,7 +26,7 @@ c2 = 0;
 
 eqtest = re.findall(r'([+-]*([\-\+]?[\ +-]?[0-9]*(\.[0-9]+)?)\ \* X\^\d+.\d+)', eq)
 
-print 'EQTEST ------->', eqtest
+#print 'EQTEST ------->', eqtest
 if eqtest:
     print 'Please verify your input: It seems you power have coma). Exit\n'
     sys.exit()
@@ -34,7 +34,7 @@ if eqtest:
 
 def check_power(eq1):
     for key in eq1:
-        print 'KEY-------->',  float(key[0][len(key[0]) - 1])
+        #print 'KEY-------->',  float(key[0][len(key[0]) - 1])
 
         if float(key[0][len(key[0]) - 1]) > 2:
             print 'The polynomial degree is stricly greater than 2, I can\'t solve.'
@@ -51,7 +51,7 @@ def check_consistency(eq1, av):
         sys.exit()
 eq1 = re.findall(r'([+-]*([\-\+]?[\ +-]?[0-9]*(\.[0-9]+)?)\ \* X\^\d+)', eq)
 
-print '\neq1 =>', eq1, '\n'
+#print '\neq1 =>', eq1, '\n'
 check_power(eq1)
 check_consistency(eq1, sys.argv[1])
 
@@ -60,7 +60,7 @@ if not eq1:
     print 'Please verify your input. Exit\n'
     sys.exit();
 
-print '\neq1 =>', eq1, '\n'
+#print '\neq1 =>', eq1, '\n'
 
 def search_equal(eq):
     i = 0
@@ -79,7 +79,7 @@ def push_deg(member_full, deg, pos, neg):
     global c1
     global c2
     re = member_full[1].strip(' ')
-    print 'After strip ... ->' + re
+    #print 'After strip ... ->' + re
     re = float(re)
     #print 'After re ... ->' , re
     #print 'POS ->>>', pos
@@ -129,9 +129,6 @@ def print_polynomial_degree(c0, c1, c2):
         degree = 3
     return degree
 
-#test
-#python computor.py "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 0"
-
 def get_delta(c0, c1, c2):
     #delta = b^2 - 4 * ac = c1^2 - 4 * c2 * c0
     #print c0
@@ -149,7 +146,7 @@ while (i < len(eq1)):
     neg = 0
     deg = 0
     equal_pos += len(eq1[i][0])
-    #print 'LENNNNN.............', len(eq1[i][0])
+    #print 'LEN =', len(eq1[i][0])
     if eq1[i][0][0] == '+':
         pos = 1
         eq2.append('pos')
@@ -160,10 +157,10 @@ while (i < len(eq1)):
         pos = 1
         eq2.append('pos')
     deg = get_deg(eq1[i][0])
-    #print 'deg => ', deg
-    #print 'I =', i
-    #print 'EQUAL = ', equal
-    #print 'EQUAL_POS = ', equal_pos
+    #print 'deg = ', deg
+    #print 'i =', i
+    #print 'equal =', equal
+    #print 'equal_pos = ', equal_pos
     if equal_pos < equal:
         push_deg(eq1[i], deg, pos, neg)
     else:
@@ -179,18 +176,18 @@ while (i < len(eq1)):
     neg = 0
     i += 1
 
-print '\n', eq2
+#print '\n', eq2
 
 print 'Reduced form:',
 print_reduced(c0, c1, c2)
 if print_polynomial_degree(c0, c1, c2) != 3:
     print 'Polynomial degree: ', print_polynomial_degree(c0, c1, c2)
 delta = get_delta(c0, c1, c2)
-print 'Delta =>', delta
+#print 'Delta =>', delta
 if delta > 0 and print_polynomial_degree(c0, c1, c2) == 2:
     print 'Discriminant is strictly positive, the two solutions are: '
-    print (-c1 + (delta**(1/2)))/(2 * c2)
-    print (-c1 - (delta**(1/2)))/(2 * c2)
+    print (-c1 + (sqrt(delta)))/(2 * c2)
+    print (-c1 - (sqrt(delta)))/(2 * c2)
 elif delta == 0 and print_polynomial_degree(c0, c1, c2) == 2:
     print 'Discriminant is equal to zero, the double real solution is: '
     print -c1/(2*c2)
@@ -206,6 +203,6 @@ elif print_polynomial_degree(c0, c1, c2) == 0 and c0 != 0:
 elif print_polynomial_degree(c0, c1, c2) == 3 or (print_polynomial_degree(c0, c1, c2) == 0 and c0 == 0):
     print 'There an infinite number of solutions to this equation'
 
-print '\nC0 => ', c0
-print 'C1 => ', c1
-print 'C2 => ', c2
+#print '\nC0 => ', c0
+#print 'C1 => ', c1
+#print 'C2 => ', c2
